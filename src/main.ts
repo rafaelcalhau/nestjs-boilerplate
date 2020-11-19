0dec05fb5a440dce0f1655a0095c792744fcc2fc
Rafael Calhau <rafael@calhau.me>
2020-11-19 15:39:02 -0300

    Added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('nestjs-boilerplate')
+    .setDescription('Just a boilerplate to build amazing backend apps with NestJS.')
+    .setVersion('1.0')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('explore', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
